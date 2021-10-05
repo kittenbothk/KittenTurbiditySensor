85b914124a9c03ded39070783ec18889570f731d
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2021-10-05 11:09:42 +0800

    calibration

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color="#fcba03" weight=10 icon="\uf2ce"
 //% groups='["KittenTurbidity"]'
 namespace KittenTurbidity {
-let temp=25.0
+let temp=25
 let deltaU=0
 let U=0
 let U25=0
@@ -15,10 +15,24 @@ let x=0
         return 10
     }
     
-    //% blockId=calibrate block="calibrate %t, pin %pin"
+    //% blockId=calibrate block="Calibrate Temp %t, pin %pin"
     //% group="Calibration" weight=83
     export function Calibrate(t: number, pin: AnalogPin) {
-        temp=t
+        temp = t
         x = pins.analogReadPin(pin)
+        deltaU = -0.0192*(temp-25)
+        U = x*5/1024
+        U25 = U-deltaU
+        K = 865.68*U25
+    }
+    
+    //% blockId=calibrate_notemp block="Calibrate No Temp pin %pin"
+    //% group="Calibration" weight=83
+    export function Calibrate_notemp(pin: AnalogPin) {
+        x = pins.analogReadPin(pin)
+        deltaU = -0.0192*(temp-25)
+        U = x*5/1024
+        U25 = U-deltaU
+        K = 865.68*U25
     }
 }
