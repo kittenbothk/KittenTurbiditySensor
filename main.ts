8cd832c0506f6eef4529d76b83e60e114f161353
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2021-10-05 10:42:45 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
-basic.forever(function () {
-	
-})
+//% color="#fcba03" weight=10 icon="\uf2ce"
+//% groups='["KittenTurbidity"]'
+namespace KittenTurbidity {
+let temp=25.0
+let deltaU=0
+let U=0
+let U25=0
+let K=0
+let TU=0
+let x=0
+
+    //% blockId=test block=test"
+    //% group="TEST" weight=83
+    export function Test(): number {
+        return 10
+    }
+}
