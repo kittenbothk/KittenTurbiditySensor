cbdfe0ef2f119638b652ed4e8bde892b5d88e718
kittenbothk <59426624+kittenbothk@users.noreply.github.com>
2021-10-05 17:01:42 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -48,6 +48,7 @@ let x=0
     //% group="Water Temperature" weight=83
     export function water_temp(p: temppin): number {
         temp=Math.round(Temperature(p)/10)
+        basic.pause(100)
         temp=Math.round(Temperature(p)/10)
         return temp
     }
